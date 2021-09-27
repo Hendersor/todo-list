f91246589e423b3a492bdc0883c84a5bbdf94965
Francisco <montejof1998@gmail.com>
2021-09-27 13:56:35 -0700

    Limpiamos el input al introducir un objeto a la lista

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ let task = document.getElementById('input');
 task.addEventListener('keyup', input);
 
 
-
-
 function input (event){
 const code = event.keyCode;
 const taskValue = task.value;
    if(code === 13){
+       if(task.value.length > 0){
         const area = document.querySelector('ul.list');
         const li = document.createElement('li');
         area.appendChild(li);
         const userTask = document.createTextNode(taskValue);
         li.appendChild(userTask);
-        console.log(taskValue);
+        deleteInput();
+       }
+
    }
 }
+
+function deleteInput(){
+    let task = document.getElementById('input').value = '';
+
+}
+
 
