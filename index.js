fef807b51a5cbcc72b0b33aa3aec9881c6e5b532
Francisco <montejof1998@gmail.com>
2021-09-27 12:56:19 -0700

    It's alivee!

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-let task = document.getElementById('btn');
-task.addEventListener('click', boton);
+let task = document.getElementById('input');
+task.addEventListener('keyup', input);
 
-function boton(){
-    console.log("Hola");
+
+
+
+function input (event){
+const code = event.keyCode;
+const taskValue = task.value;
+   if(code === 13){
+        const area = document.querySelector('ul.list');
+        const li = document.createElement('li');
+        area.appendChild(li);
+        const userTask = document.createTextNode(taskValue);
+        li.appendChild(userTask);
+        console.log(taskValue);
+   }
 }
+
